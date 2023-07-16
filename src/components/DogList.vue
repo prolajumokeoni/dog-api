534144f3f4175c6bc381fa88aae4a951b9fa64a1
prolajumokeoni <prolajumokeoni@gmail.com>
2023-07-16 15:01:14 +0100

    Style Doglist and implement lazy loading using Intersection observer

--- src/components/DogList.vue
+++ src/components/DogList.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
-    <ul v-if="!loading" ref="dogList">
-      <li v-for="(breed, index) in dogData" :key="index" >
+  <div class="dog-container">
+    <ul v-if="!loading" class="dog-list" ref="dogList">
+      <li v-for="(breed, index) in dogData" :key="index" class="dog-item">
         <router-link :to="`/dog/${breed.name}`">
-          <div>
-            <img :src="breed.image" :alt="breed.name"  />
+          <div class="dog-image-container">
+            <img :src="breed.image" :alt="breed.name" ref="dogImages" class="dog-image" />
           </div>
           <span>{{ breed.name }}</span>
         </router-link>
@@ -15,6 +15,46 @@
     </div>
   </div>
 </template>
+
+<style>
+.dog-container {
+  display: flex;
+  justify-content: center;
+}
+
+.dog-list {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.dog-item {
+  width: 20%;
+  max-width: 200px;
+  margin: 10px;
+  text-align: center;
+}
+
+.dog-image-container {
+  width: 100%;
+  height: 0;
+  padding-bottom: 100%;
+  position: relative;
+  overflow: hidden;
+}
+
+.dog-image {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
 
 <script>
 import { ref, onMounted } from 'vue';
@@ -45,6 +85,23 @@ export default {
         console.error('Error fetching dog data:', error);
         loading.value = false;
       }
+    });
+
+    onMounted(() => {
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const img = entry.target;
+            img.src = img.dataset.src;
+            observer.unobserve(img);
+          }
+        });
+      });
+
+      const dogImages = Array.from(document.querySelectorAll('[ref="dogImages"]'));
+      dogImages.forEach((img) => {
+        observer.observe(img);
+      });
     });
 
     return {
