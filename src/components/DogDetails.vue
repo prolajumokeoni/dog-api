a5b78988b14c34ade87bafc36a29d1e4895dcdfd
prolajumokeoni <prolajumokeoni@gmail.com>
2023-07-17 09:55:21 +0100

    Setup Coudinary  and add filters for images

--- src/components/DogDetails.vue
+++ src/components/DogDetails.vue
@@ -1,13 +1,23 @@
 <template>
-  <div v-if="loading" class="loader-container"><div class="loader"></div></div>
+  <div v-if="loading" class="loader-container">
+    <div class="loader"></div>
+  </div>
   <div v-else>
     <h2>{{ breed }}</h2>
-    <img :src="dogData.image" :alt="breed" />
+    
+    <img :src="getTransformedUrl(dogData.image, selectedFilter)" :alt="breed" />
+    
+    <div>
+      <button @click="applyFilter('cartoonify')">Cartoonify</button>
+      <button @click="applyFilter('grayscale')">Grayscale</button>
+      <button @click="applyFilter('sepia')">Sepia</button>
+      <button @click="resetFilter">Reset</button>
+    </div>
   </div>
 </template>
 
 <script>
-import { computed, onMounted } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
 import { useStore } from 'vuex';
 
@@ -16,6 +26,7 @@ export default {
     const store = useStore();
     const route = useRoute();
     const breed = route.params.breed;
+    const selectedFilter = ref(null); 
 
     onMounted(() => {
       store.dispatch('fetchDogData');
@@ -28,13 +39,43 @@ export default {
 
     const loading = computed(() => store.getters.isLoading);
 
+    const cloudinaryConfig = {
+      cloudName: 'dfum4zfrp',
+      transformations: {
+        cartoonify: 'e_cartoonify',
+        grayscale: 'e_grayscale',
+        sepia: 'e_sepia',
+      },
+    };
+
+    const getTransformedUrl = (imageUrl, filter) => {
+      if (filter && cloudinaryConfig.transformations[filter]) {
+        return `https://res.cloudinary.com/${cloudinaryConfig.cloudName}/image/upload/${cloudinaryConfig.transformations[filter]}/${imageUrl}`;
+      } else {
+        return imageUrl;
+      }
+    };
+
+    const applyFilter = (filter) => {
+      selectedFilter.value = filter;
+    };
+
+    const resetFilter = () => {
+      selectedFilter.value = null;
+    };
+
     return {
       breed,
       dogData,
-      loading
+      loading,
+      selectedFilter,
+      getTransformedUrl,
+      applyFilter,
+      resetFilter,
     };
-  }
+  },
 };
 </script>
 
- 
+<style>
+</style>
